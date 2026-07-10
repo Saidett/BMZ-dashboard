5fc229910e62a3abf9a42813e6885775e015bc3d
Saidett <66988743+Saidett@users.noreply.github.com>
2026-07-10 09:34:39 +0200

    Updated download and better chunking
    Added chunking with extensive cleaning of pdfs pre- and post chunking. Using fitz to handle column-based pdfs better. Chunks are split recursively with langchain text splitter.

--- python/extract/parse.py
+++ python/extract/parse.py
@@ -1,28 +1,63 @@
 import pdfplumber
 from pathlib import Path
 from python.config import DATA_RAW
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import fitz
+import re
+import html
 
 pdf_dir = DATA_RAW
 all_chunks = []
 
-def chunk_text(text, size=500, overlap=50):
-    words = text.split()
-    chunks = []
-    for i in range(0, len(words), size - overlap):
-        chunks.append(" ".join(words[i:i + size]))
-    return chunks
+# using langchain recursive splitting to respect PDF structure
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,   
+    chunk_overlap=50
+)
 
+# pre-cleaning helper function
+def clean_text(text):
+    text = html.unescape(text)  # remove html chars            
+    text = re.sub(r'[ \t]+', ' ', text)     # remove double spaces
+    text = re.sub(r'\n{3,}', '\n\n', text)  # remove excessive line breaks
+    text = re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f]', '', text)  # remove special chars
+    text = text.replace('\xad', '')           # remove soft hyphens
+    text = re.sub(r'-\n', '', text)           # join hyphenated line breaks
+    text = re.sub(r'(?<=\w)\n(?=\w)', '', text)  # merge line break if next to them its not whitespace (to merge hyphenated words)
+    text = re.sub(r'(?<!\n)\n(?!\n)', ' ', text)  # single \n → space, keep \n\n
+    return text.strip(" \t")
+
+# post cleaning helper function
+def final_clean(chunk):
+    chunk = chunk.strip().replace('\n', ' ')
+    chunk = re.sub(r' +', ' ', chunk)
+    return chunk
+
+# loop through PDFs to turn into chunks and save them in all_chunks list
 for pdf_path in sorted(pdf_dir.glob("*.pdf")):
-    with pdfplumber.open(pdf_path) as pdf:
-        text = "\n".join(page.extract_text() or "" for page in pdf.pages)
     
-    chunks = chunk_text(text)
+    # using fitz to read columns properly
+    doc = fitz.open(pdf_path)
+    text = ""
+        
+    for page in doc:
+        # gets blocks per page with coordinates
+        blocks = page.get_text("blocks")
+        # sorting blocks to sort text
+        blocks.sort(key=lambda b: (b[1], b[0]))
+        for b in blocks:
+            if b[6] == 0:  # to keep only text blocks
+                text += b[4] + "\n"
+    
+    # clean text before chunking
+    text = clean_text(text)
+
+    # now do the recursive chunking with langchain
+    chunks = splitter.split_text(text)
     
     for chunk in chunks:
         all_chunks.append({
             "pdf": pdf_path.name,
-            "chunk": chunk,
+            "chunk": final_clean(chunk),
             "char_count": len(chunk),
         })
-
-
