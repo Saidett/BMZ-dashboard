b28ba361639b5bb665c734f8ab15b51e7e3ab9c1
Saidett <66988743+Saidett@users.noreply.github.com>
2026-08-05 10:29:53 +0200

    Using a new publications URL
    - Changed towards proper publications API
    - Using both evaluation and strategy documents that are in German

--- python/extract/01-download.py
+++ python/extract/01-download.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 from PyPDF2 import PdfReader
 import os
 from python.config import DATA_RAW
+import time
+import pickle
 
 # publications base url
 base_url = "https://www.transparenzportal.bund.de/api/v1/publications?"
@@ -10,41 +12,40 @@ base_url = "https://www.transparenzportal.bund.de/api/v1/publications?"
 # get the json with publications
 read = requests.get(base_url).json()
 
-# how many hits?
-read["meta"]["count"]
+# keep only documents with strategy, evaluations and results
+document_categories = ["B11", "B02", "B04", "B12", "B01", "A04", "A08", "A07", "B09"]
 
-# keep only documents with strategy
-document_categories = ["B11", "B02", "B04", "B12", "B01", "B18"]
+# keep only German-language publications and only pdfs
+german_pubs = [p for p in read["data"] if p["language"] == "de" and p["category"] in document_categories and ".pdf" in p["url"]]
+len(german_pubs)
 
-# keep only German-language publications
-german_pubs = [p for p in read["data"] if p["language"] == "de" and p["category"] in document_categories]
+# remove irrelevant keys
+for d in german_pubs:
+    d.pop("mimetype", None)
 
-urls = [dict["url"] for dict in german_pubs]
-
-# define list to save results
-publications_list = []
-
-# cycle through base url with offset and limit at 9, then append
-for offset in range(0, nr_publications, 9):
-    url = f"{base_url}?limit=9&offset={offset}"
-    read = requests.get(url)
-    soup = BeautifulSoup(read.content, "html.parser")
+for publication in german_pubs:
     
-    links = soup.find_all("a", class_="a-publication-button a-publication-button--with-clickarea")
-    hrefs = [link.get("href") for link in links]
-    publications_list.extend(hrefs)
-
-# remove kinderbuch and stickerbuch and kurzfassung
-terms_list = ["kinderbuch", "stickerbuch", "kinderplakat", "kurzfassung", "umwelterklaerung"]
-filtered_list = [url for url in publications_list 
-            if not any(term in url.lower() for term in terms_list)]
-
-print(len(filtered_list))
-
-for pdf_file in filtered_list:
-    file_path = os.path.join(DATA_RAW, pdf_file.split('/')[-1])
-    response = requests.get(pdf_file)
+    url = publication["url"]
+    file_path = os.path.join(DATA_RAW, url.split('/')[-1])
     
+    # tries 5 times, if good response, continues, if not sleep and retry
+    for attempt in range(5):
+        try: 
+            response = requests.get(url, timeout = 120)
+            break
+        except requests.exceptions.ChunkedEncodingError:
+            time.sleep(2 ** attempt)
+
     file = open(file_path, "wb")
     file.write(response.content)
     file.close()
+
+    time.sleep(0.2)
+
+# append file path to each dict in publication list
+for pub in german_pubs:
+    pub["path"] = os.path.join(DATA_RAW, pub["url"].split('/')[-1])
+
+# save file with publication meta data
+with open("data/processed/publication_meta.json", "w", encoding = "utf-8") as f:
+    json.dump(german_pubs, f, ensure_ascii = False, indent = 2)
